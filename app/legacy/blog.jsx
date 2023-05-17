d3344a629dfda3e022cf9e1d1046c9237a45e7c8
xuannghia <xuannghia197@gmail.com>
2023-05-17 22:15:04 +0700

    chore: fix eslint & unimported

--- app/legacy/blog.jsx
+++ app/legacy/blog.jsx
@@ -1,5 +1,6 @@
 import {Box} from '@mui/material';
 import {useParams} from 'react-router';
+import Button from '~/components/button';
 
 export default function BlogPage() {
 	const params = useParams();
@@ -8,6 +9,7 @@ export default function BlogPage() {
 		<Box px={2}>
 			<h1>Blog Page</h1>
 			<p>Slug: {params.slug}</p>
+			<Button>Button</Button>
 		</Box>
 	);
 }
